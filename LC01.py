5ed2e8893ba792f2bdc4ffefb257d4ee002b27b7
Shpoogtaz <93548948+Shpoogatz@users.noreply.github.com>
2023-05-08 21:53:58 -0400

    Two Sum - LeetCode

---
+++ LC01.py
@@ -0,0 +1,18 @@
+# Given an array of integers nums and an integer target, return indices of the two numbers such that they add up to target.
+# You may assume that each input would have exactly one solution, and you may not use the same element twice.
+# You can return the answer in any order.
+
+def twoSum(nums: list[int], target: int) -> list[int]:
+    for i in range(len(nums)):
+        for j in range(i + 1, len(nums)):
+            if nums[i] + nums[j] == target:
+                return [i,j]
+
+# iterate over the length of the list
+# for each num in nums
+# check if any of the numbers after will sum to target
+# if not move on
+# if so return indexes of two elemens that sum to target
+
+
+        
