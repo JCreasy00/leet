50d67326a4ef996dbb5d4d4af9425bd26e96ed4e
Shpoogtaz <93548948+Shpoogatz@users.noreply.github.com>
2023-05-13 16:40:12 -0400

    Update LC35.py

--- LC35.py
+++ LC35.py
@@ -1,25 +1,26 @@
 # Given a sorted array of distinct integers and a target value, return the index if the target is found.
 # If not, return the index where it would be if it were inserted in order.
 # You must write an algorithm with O(log n) runtime complexity.
-import math 
+
+# binary search is O(log n)
 
 
-def searchInsert(self, nums: list[int], target: int) -> int:
+def searchInsert(self, nums: List[int], target: int) -> int:
+        left = 0
+        right = len(nums) - 1
+        mid = 0
 
-    left = 0
-    right = len(nums)
-    mid = 0
+        while left <= right:
+            mid = (left + right) // 2
 
-    while left <= right:
-        mid = (left + right) // 2
+            if nums[mid] < target: 
+                left = mid + 1
 
-        if nums[mid] < target: 
-            left = mid + 1
+            elif nums[mid] > target: 
+                right = mid - 1
 
-        elif nums[mid] > target: 
-            right = mid - 1
+            else: # perfect case
+                return mid
 
-        else: # perfect case
-            return mid
+        return left
             
-        
